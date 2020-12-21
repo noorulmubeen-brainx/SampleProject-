92d472cc8c2acaf08713e9a5d381c18653abdb81
Mubeen Ikram <noorulmubeen@brainxtech.com>
2020-12-21 13:23:48 +0500

    Skip button on On boarding screen is now accessed in code Rounded corners added View changed for onBoarding fragment

--- app/src/main/java/com/brainXTech/sampleapplevelup/activity/OnBoardingActivity.kt
+++ app/src/main/java/com/brainXTech/sampleapplevelup/activity/OnBoardingActivity.kt
@@ -2,6 +2,7 @@ package com.brainXTech.sampleapplevelup.activity
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
@@ -10,9 +11,6 @@ import androidx.viewpager2.widget.ViewPager2
 import com.brainXTech.sampleapplevelup.R
 import com.brainXTech.sampleapplevelup.adapters.OnBoardingPageAdapter
 import com.brainXTech.sampleapplevelup.databinding.ActivityOnBoardingBinding
-import com.google.android.material.tabs.TabLayout
-import kotlinx.android.synthetic.main.on_boarding_bottom_dots_unfilled.*
-import com.google.android.material.tabs.TabLayoutMediator
 
 class OnBoardingActivity : FragmentActivity(),View.OnClickListener, ViewPager.OnPageChangeListener{
     private lateinit var onBoardingBinding: ActivityOnBoardingBinding
@@ -21,6 +19,7 @@ class OnBoardingActivity : FragmentActivity(),View.OnClickListener, ViewPager.On
     private  lateinit var firstDot:ImageView
     private  lateinit var secondDot:ImageView
     private  lateinit var thirdDot:ImageView
+    private lateinit var skipButton: Button
 
 
     override fun onClick(v: View?) {
@@ -29,6 +28,20 @@ class OnBoardingActivity : FragmentActivity(),View.OnClickListener, ViewPager.On
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setBindings()
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        viewPager2.addOnPageChangeListener(this)
+        skipButton.setOnClickListener(this)
+        firstDot.setOnClickListener(this)
+        secondDot.setOnClickListener(this)
+        thirdDot.setOnClickListener(this)
+    }
+
+    private fun setBindings() {
         onBoardingBinding=DataBindingUtil.setContentView(this,R.layout.activity_on_boarding)
         viewPager2=onBoardingBinding.onBoardingViewPager
         pagerAdapter=OnBoardingPageAdapter(supportFragmentManager)
@@ -36,9 +49,9 @@ class OnBoardingActivity : FragmentActivity(),View.OnClickListener, ViewPager.On
         firstDot=onBoardingBinding.firstDot
         secondDot=onBoardingBinding.secondDot
         thirdDot=onBoardingBinding.thirdDot
-        viewPager2.addOnPageChangeListener(this)
-
+        skipButton=onBoardingBinding.skipButton
     }
+
     fun setSelectedDot(position: Int) {
         when(position){
             0-> firstDot.setBackgroundResource(R.drawable.ic_selected_bottom_point)
@@ -54,27 +67,27 @@ class OnBoardingActivity : FragmentActivity(),View.OnClickListener, ViewPager.On
         setSelectedDot(position)
     }
 
-     private val onPageChange=object : ViewPager2.OnPageChangeCallback() {
-        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-        }
-        override fun onPageSelected(position: Int) {
-            super.onPageSelected(position)
-            firstDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
-            secondDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
-            thirdDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
-//            resetDotsSource(position)
-            when(position){
-                0-> firstDot.setImageResource(R.drawable.ic_selected_bottom_point)
-                1->secondDot.setImageResource(R.drawable.ic_selected_bottom_point)
-                2->thirdDot.setImageResource(R.drawable.ic_selected_bottom_point)
-            }
-
-        }
-        override fun onPageScrollStateChanged(state: Int) {
-            super.onPageScrollStateChanged(state)
-        }
-    }
+//     private val onPageChange=object : ViewPager2.OnPageChangeCallback() {
+//        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+//            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+//        }
+//        override fun onPageSelected(position: Int) {
+//            super.onPageSelected(position)
+//            firstDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
+//            secondDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
+//            thirdDot.setImageResource(R.drawable.ic_not_selected_bottom_point)
+////            resetDotsSource(position)
+//            when(position){
+//                0-> firstDot.setImageResource(R.drawable.ic_selected_bottom_point)
+//                1->secondDot.setImageResource(R.drawable.ic_selected_bottom_point)
+//                2->thirdDot.setImageResource(R.drawable.ic_selected_bottom_point)
+//            }
+//
+//        }
+//        override fun onPageScrollStateChanged(state: Int) {
+//            super.onPageScrollStateChanged(state)
+//        }
+//    }
 
     override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
 //        TODO("Not yet implemented")
